d489f6d15c288d7880a11ab8400528f377a29ea5
kyaloeriq <kyaloeriq@gmail.com>
2024-07-22 12:20:11 -0700

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    return [true, false];
+    task = true;
+    task2 = false;	
   }
 
   return [task, task2];

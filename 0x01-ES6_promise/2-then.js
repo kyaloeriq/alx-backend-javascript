0b67b260d9b906cc1e9162c6c4ab831a3356d073
kyaloeriq <kyaloeriq@gmail.com>
2024-07-24 11:23:55 -0700

    Appending handlers to a function

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,15 @@
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      return {
+        status: 200,
+        body: 'success'
+      };
+    })
+    .catch(() => {
+      return new Error();
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}

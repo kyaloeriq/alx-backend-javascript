190e232a86d50fa6d323c982f7908d003b477fe9
kyaloeriq <kyaloeriq@gmail.com>
2024-07-25 11:04:26 -0700

    Importing the ClassRoom class from 0-classroom.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,17 +1,8 @@
-// Import the ClassRoom class from 0-classroom.js
 import ClassRoom from './0-classroom.js';
 
-/**
- * Initializes an array of ClassRoom objects with specified sizes.
- * @returns {ClassRoom[]} Array of ClassRoom objects with sizes 19, 20, and 34.
- */
 function initializeRooms() {
-  return [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34),
-  ];
+  const roomSizes = [19, 20, 34];
+  return roomSizes.map(size => new ClassRoom(size));
 }
 
-// Export the initializeRooms function for use in other modules
-export { initializeRooms };
+export default initializeRooms;

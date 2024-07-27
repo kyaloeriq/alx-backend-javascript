f856262728ccc4ea8fa15377e3e86f3fc43ac140
kyaloeriq <kyaloeriq@gmail.com>
2024-07-27 09:17:26 -0700

    a class named HolbertonCourse

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = this._validateName(name);
-    this._length = this._validateLength(length);
-    this._students = this._validateStudents(students);
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   // Getter for name
@@ -12,7 +12,10 @@ class HolbertonCourse {
 
   // Setter for name with type check
   set name(value) {
-    this._name = this._validateName(value);
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
   }
 
   // Getter for length
@@ -22,7 +25,10 @@ class HolbertonCourse {
 
   // Setter for length with type check
   set length(value) {
-    this._length = this._validateLength(value);
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
   }
 
   // Getter for students
@@ -32,31 +38,10 @@ class HolbertonCourse {
 
   // Setter for students with type check
   set students(value) {
-    this._students = this._validateStudents(value);
-  }
-
-  // Validation method for name
-  _validateName(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    return value;
-  }
-
-  // Validation method for length
-  _validateLength(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    return value;
-  }
-
-  // Validation method for students
-  _validateStudents(value) {
     if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
-    return value;
+    this._students = value;
   }
 }
 
@@ -72,3 +57,10 @@ course.students = ['Dave', 'Eva'];
 console.log(course.name); // Output: Python
 console.log(course.length); // Output: 14
 console.log(course.students); // Output: ['Dave', 'Eva']
+
+// Uncomment the following lines to see the type checking in action
+// course.name = 123; // Throws TypeError: Name must be a string
+// course.length = 'long'; // Throws TypeError: Length must be a number
+// course.students = 'Alice'; // Throws TypeError: Students must be an array of strings
+// course.students = [123, 'Bob']; // Throws TypeError: Students must be an array of strings
+

1f232769bab8199e9c3a42df7df6a80f7692cd51
kyaloeriq <kyaloeriq@gmail.com>
2024-07-30 11:04:59 -0700

    returns an array of ids from a list of object

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,8 +2,10 @@ function getListStudentIds(students) {
   if (!Array.isArray(students)) {
     return [];
   }
-  
-  return students.map(student => student.id);
+
+  return students
+    .filter(student => student && student.id !== undefined)
+    .map(student => student.id);
 }
 
 module.exports = getListStudentIds;

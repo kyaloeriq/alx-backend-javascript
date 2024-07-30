0aae14b827ff6da645d544bf1253db3da40bb46d
kyaloeriq <kyaloeriq@gmail.com>
2024-07-30 10:17:26 -0700

    returns an array of students for a specific city with their new grade

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,20 @@
+/**
+ * Updates the grades of students by city.
+ * @param {Array} students - The list of students.
+ * @param {string} city - The city to filter students by.
+ * @param {Array} newGrades - The new grades to be assigned.
+ * @returns {Array} - The list of students with updated grades.
+ */
+function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter(student => student.location === city)
+    .map(student => {
+      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A'
+      };
+    });
+}
+
+module.exports = updateStudentGradeByCity;

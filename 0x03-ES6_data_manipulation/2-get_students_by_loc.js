314eee307792f98da0c75211dbb9c7c511e6da32
kyaloeriq <kyaloeriq@gmail.com>
2024-07-30 09:59:38 -0700

    returns an array of objects who are located in a specific city

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,9 @@
+/**
+ * Filters students by location.
+ * @param {Array} students - The list of students.
+ * @param {string} city - The city to filter students by.
+ * @returns {Array} - The list of students located in the specified city.
+ */
 function getStudentsByLocation(students, city) {
   return students.filter(student => student.location === city);
 }

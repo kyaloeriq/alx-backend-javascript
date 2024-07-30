5c4c299f624eef6cded4ca0efd2f4f0444d9eee7
kyaloeriq <kyaloeriq@gmail.com>
2024-07-30 10:55:47 -0700

    returns a new ArrayBuffer with an Int8 value at a specific position

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,21 @@
+/**
+ * Creates a new ArrayBuffer with an Int8 value at a specific position.
+ * @param {number} length - The length of the ArrayBuffer.
+ * @param {number} position - The position in the buffer to set the value.
+ * @param {number} value - The Int8 value to set at the specified position.
+ * @returns {Uint8Array} - The typed array view of the ArrayBuffer.
+ * @throws {Error} - Throws an error if the position is outside the range.
+ */
+function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+  const int8View = new Int8Array(buffer);
+  int8View[position] = value;
+
+  return int8View;
+}
+
+module.exports = createInt8TypedArray;

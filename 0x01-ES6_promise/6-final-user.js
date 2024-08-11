79aa687a27e422263a971bbb327d787d9515d3d8
kyaloeriq <kyaloeriq@gmail.com>
2024-08-11 10:15:55 -0700

    Handling multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,22 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
-  ]).then((results) => results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  })));
+/**
+ * Handles profile signup by calling signUpUser and uploadPhoto.
+ * 
+ * @param {string} firstName - The first name of the user.
+ * @param {string} lastName - The last name of the user.
+ * @param {string} fileName - The name of the photo file to upload.
+ * @returns {Promise<Array>} - A promise that resolves to an array with the results of both promises.
+ */
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+    const signUpPromise = signUpUser(firstName, lastName);
+    const uploadPhotoPromise = uploadPhoto(fileName);
+
+    const results = await Promise.allSettled([signUpPromise, uploadPhotoPromise]);
+
+    return results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : `${result.reason}`,
+    }));
 }

411456173c577ddda6d40c68e293d632ca2cf3f2
kyaloeriq <kyaloeriq@gmail.com>
2024-08-13 10:02:54 -0700

    Implementing a class named Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -13,6 +13,7 @@ class Building {
   }
 
   // Ensure that any class extending Building implements evacuationWarningMessage
+  // eslint-disable-next-line class-methods-use-this
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }

b1f1f212980d4817a580fb3de6c97f54aeef8f0b
kyaloeriq <kyaloeriq@gmail.com>
2024-08-13 09:23:40 -0700

    Implementing a class named Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,20 +1,20 @@
 // Define the Building class as an abstract class with required methods and attributes.
 class Building {
-    constructor(sqft) {
-        if (new.target === Building) {
-            throw new TypeError('Cannot construct Building instances directly');
-        }
-        this._sqft = sqft;
+  constructor(sqft) {
+    if (new.target === Building) {
+      throw new TypeError('Cannot construct Building instances directly');
     }
+    this._sqft = sqft;
+  }
 
-    get sqft() {
-        return this._sqft;
-    }
+  get sqft() {
+    return this._sqft;
+  }
 
-    // Ensure that any class extending Building implements evacuationWarningMessage
-    evacuationWarningMessage() {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+  // Ensure that any class extending Building implements evacuationWarningMessage
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
 }
 
 module.exports = Building;

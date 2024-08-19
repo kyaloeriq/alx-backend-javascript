ffd3564530b97824109e942c45cc4fff1f997a13
kyaloeriq <kyaloeriq@gmail.com>
2024-08-19 14:36:25 -0700

    a class named Pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -2,34 +2,34 @@
 const Currency = require('./3-currency.js');
 
 class Pricing {
-    constructor(amount, currency) {
-        this._amount = amount;
-        this._currency = currency;
-    }
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-    get amount() {
-        return this._amount;
-    }
+  get amount() {
+    return this._amount;
+  }
 
-    set amount(amount) {
-        this._amount = amount;
-    }
+  set amount(amount) {
+    this._amount = amount;
+  }
 
-    get currency() {
-        return this._currency;
-    }
+  get currency() {
+    return this._currency;
+  }
 
-    set currency(currency) {
-        this._currency = currency;
-    }
+  set currency(currency) {
+    this._currency = currency;
+  }
 
-    displayFullPrice() {
-        return `${this._amount} ${this._currency.displayFullCurrency()}`;
-    }
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
 
-    static convertPrice(amount, conversionRate) {
-        return amount * conversionRate;
-    }
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
 
 module.exports = Pricing;

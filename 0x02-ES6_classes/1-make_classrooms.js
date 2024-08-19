5ffcbdb42b079b9757d8d5dbbf4a04f9abe6f953
kyaloeriq <kyaloeriq@gmail.com>
2024-08-19 12:39:03 -0700

    function named initializeRooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,8 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 function initializeRooms() {
   const roomSizes = [19, 20, 34];
-  return roomSizes.map(size => new ClassRoom(size));
+  return roomSizes.map((size) => new ClassRoom(size));
 }
 
 export default initializeRooms;

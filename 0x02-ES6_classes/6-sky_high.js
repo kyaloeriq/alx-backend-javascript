1ce7aa360f54166d8845e886c778348f6fcc3427
kyaloeriq <kyaloeriq@gmail.com>
2024-08-19 17:24:21 -0700

    Inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,23 @@
+// Import the Building class
+import Building from './5-building.js';
+
+// Define the SkyHighBuilding class that extends Building
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    // Call the parent class constructor with sqft
+    super(sqft);
+    this._floors = floors;
+  }
+
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  // Override the evacuationWarningMessage method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
+
+export default SkyHighBuilding;

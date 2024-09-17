11f62d3df08bb6323d5bc41b56cbad52351b070b
kyaloeriq <kyaloeriq@gmail.com>
2024-09-17 08:55:47 +0000

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+// Program to interact with user through command line
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+// Listen for user input
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  
+  // Close the input stream after receiving the input
+  process.stdin.end();
+});
+
+// Handle program exit
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});

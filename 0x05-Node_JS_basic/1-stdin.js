f9b5ee8e7a8c58ac2675ac090615063abac97f76
kyaloeriq <kyaloeriq@gmail.com>
2024-09-18 05:34:42 +0000

    1-stdin.js that will be executed through command line

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,18 +2,22 @@
 
 // Program to interact with user through command line
 
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+function displayMessage() {
+  process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Listen for user input
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  
-  // Close the input stream after receiving the input
-  process.stdin.end();
-});
+  // Listen for user input
+  process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
+    process.stdout.write(`Your name is: ${name}\n`);
+    
+    // Close the input stream after receiving the input
+    process.stdin.end();
+  });
 
-// Handle program exit
-process.stdin.on('end', () => {
-  console.log('This important software is now closing');
-});
+  // Handle program exit
+  process.stdin.on('end', () => {
+    console.log('This important software is now closing');
+  });
+}
+
+module.exports = displayMessage;

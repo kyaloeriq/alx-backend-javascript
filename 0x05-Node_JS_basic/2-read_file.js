478c453cb4da32559c1caaef964d2a59b069157c
kyaloeriq <kyaloeriq@gmail.com>
2024-09-18 05:50:58 +0000

    function countStudents in the file 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 
+// Import required 'fs' module
 const fs = require('fs');
 
+// Function to count students from a CSV file
 function countStudents(path) {
   try {
     // Read the file synchronously
     const data = fs.readFileSync(path, 'utf-8');
 
     // Split the file into lines and filter out empty lines
-    const lines = data.trim().split('\n').filter(line => line);
+    const lines = data.trim().split('\n').filter((line) => line);
 
     // Check if there are lines other than the header
     if (lines.length <= 1) {
@@ -17,7 +19,6 @@ function countStudents(path) {
     }
 
     // Process CSV header and student data
-    const header = lines[0].split(',');
     const students = lines.slice(1);
 
     // Total number of students
@@ -28,7 +29,7 @@ function countStudents(path) {
 
     // Loop through each student and group them by field
     students.forEach((student) => {
-      const [firstname, lastname, age, field] = student.split(',');
+      const [firstname, , , field] = student.split(',');
 
       if (!fieldGroups[field]) {
         fieldGroups[field] = [];
@@ -39,14 +40,15 @@ function countStudents(path) {
     });
 
     // Output the number of students in each field and their names
-    for (const field in fieldGroups) {
+    Object.keys(fieldGroups).forEach((field) => {
       const studentsInField = fieldGroups[field];
       console.log(`Number of students in ${field}: ${studentsInField.length}. List: ${studentsInField.join(', ')}`);
-    }
+    });
   } catch (error) {
     // If the file cannot be read, throw an error
     throw new Error('Cannot load the database');
   }
 }
 
+// Export the function to be used in other files
 module.exports = countStudents;

d87b21aaad8f4d6e4f4245929be57696ad63a52b
kyaloeriq <kyaloeriq@gmail.com>
2024-09-21 15:07:04 +0000

    utils.js file

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import path from 'path';
+
+export function readDatabase(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const lines = data.trim().split('\n').filter((line) => line !== '');
+
+        if (lines.length <= 1) {
+          reject(new Error('Cannot load the database'));
+          return;
+        }
+
+        const fields = lines.slice(1).map((line) => {
+          const [firstname, lastname, age, field] = line.split(',');
+          return { firstname, lastname, age, field };
+        });
+
+        const studentsByField = fields.reduce((acc, student) => {
+          if (!acc[student.field]) {
+            acc[student.field] = [];
+          }
+          acc[student.field].push(student.firstname);
+          return acc;
+        }, {});
+
+        resolve(studentsByField);
+      }
+    });
+  });
+}

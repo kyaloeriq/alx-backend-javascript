347f0d59b2f350fb2d3641fc236e22103991b217
kyaloeriq <kyaloeriq@gmail.com>
2024-09-22 14:09:16 +0000

    a complex HTTP server using Express

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
+import path from 'path';
 
-export default function readDatabase(filePath) {
+export function readDatabase(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
